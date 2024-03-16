c1a458ba8dc9332bf7b088193b747417288f75a2
BEKKIE <iambekkie99@gmail.com>
2024-03-17 04:24:53 +0500

    updated

--- String/maximunOccurance.cpp
+++ String/maximunOccurance.cpp
@@ -1,26 +1,79 @@
 #include <iostream>
+#include <string>
+
+
+
 using namespace std ;
+
+
+
+char alphaNumeric ( char ch ) {
+ 
+   if(ch >='a' && ch <= 'z' || ch >= 'A' && ch  <= 'Z' ||  ch >=  'o' && ch <= '9'  ) {
+    return ch ;
+   }
+
+}
+
+
+
+char toLower(  char ch ) {
+  if(ch >= 'a' && ch <= 'z' ) {
+    return ch ;
+  }
+  else {
+    int temp = ch - 'A' + 'a' ;
+    return temp;
+  }
+
+}
+
+bool palindromeChecker( string str ) {
+
+    int start = 0 ;
+    int end = str.size () - 1; ;
+
+
+   while(start < end) {
+
+
+    if(!alphaNumeric(str[start])) {
+        start++ ;
+    }
+     if(!alphaNumeric(str[end])) {
+        end-- ;
+    }
+
+    if(     toLower(str[start]) !=  toLower(str[end] )  ) {
+        return false ;
+    }
+    else {
+        start ++ ;
+        end -- ;
+    }
+   }
+  return true ;
+
+
+
+}
 
 
 int main() {
 
 
-    int num ;
-    int factorial = 1 ;
-    cin >> num; 
-    cout <<  "Enter Number to find factorial ";
 
-    if(num < 0) {
-        cout <<  "For neg numbers can't avail " ;
+  string str = "ma,m" ;
 
-    }else {
-        for(int i = 1 ; i <= num ; i++) {
-            factorial = factorial * i ;
-        }
-        cout << "factorial is"   <<factorial;
-    }
+  if(palindromeChecker(str)) {
+    cout << "The given string is palindrome" ;
+  }
+  else {
+    cout <<  "The Given string is not palindome" ;
+  }
+   
 
-
+  
 
 
 }
---
+++ String/palindrome2.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+#include <string>
+using namespace std ;
+
+
+
+
+     bool valid(char ch) {
+        if((ch >= 'a'  && ch <= 'z' )|| (ch >= 'A' && ch <= 'Z') || (ch >= '0' && ch <= '9') ) {
+            return true ;
+        }
+        return  false ;
+     } 
+
+     char toLowerCase( char ch )  {
+        if(ch >= 'a' && ch <= 'z' ) {
+            return ch ;
+        }
+        else {
+            int temp = ch - 'A' + 'a' ;
+            return temp ;
+        }
+
+     }
+
+
+
+    bool palindromeChecker(string temp) {
+        int start = 0 ;
+        int end = temp.size () - 1;
+        while(start <= end) {
+ 
+        if(temp[start] != temp[end]) {
+            return false ;
+        }
+        else {
+            start ++;
+            end --;
+        }
+
+        }
+      return true ;
+    }
+
+
+  bool isPalindrome( string str ) {
+
+
+   string temp = "" ;
+   for(int i = 0 ; i<str.size() ; i++ ) {
+
+    if(valid(str[i])) {
+     
+
+   temp.push_back(str[i]) ;
+
+    }
+   }
+  
+
+  for(int j = 0 ; j < temp.size() ;j++ ) {
+    temp[j] = toLowerCase(temp[j]) ;
+  }
+
+   
+   return palindromeChecker(temp) ;
+
+  }
+
+
+
+int main() {
+
+
+string str1 = "Toos hot to hoot." ;
+if( isPalindrome(str1) ) {
+    cout << "The given string is palindrome";
+}
+else {
+    cout << "not palindrome" ;
+}
+
+
+}
